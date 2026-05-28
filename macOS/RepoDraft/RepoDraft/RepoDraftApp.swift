5bcc86175f31c45ac87633349d58d3a424dfa6fc
krystoF <slama.krystof@icloud.com>
2026-05-28 21:30:16 +0200

    Og project file created

---
+++ macOS/RepoDraft/RepoDraft/RepoDraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoDraftApp.swift
+//  RepoDraft
+//
+//  Created by Kryštof Sláma on 28.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct RepoDraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
